e56cfdac3a5960ec4011925ad5091d3c525d07da
Marco Ceppi <mceppi@danya.com>
2010-07-22 12:25:40 -0400

    Make the output more "useful"

--- tests/extend-test.php
+++ tests/extend-test.php
@@ -3,9 +3,12 @@
 require_once("../lib/ContractRummy.php");
 
 $game = new ContractRummy();
-
 $game->shuffle();
 
+echo "<pre>";
+echo "Total cards in deck: " . $game->total() . "\n";
 print_r($game->deal());
+echo "Total cards in deck: " . $game->total();
+echo "</pre>";
 
 ?>

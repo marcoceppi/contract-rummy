6e765d496173dc5e1de64b085e95eb25b6043861
Marco Ceppi <mceppi@danya.com>
2010-07-22 10:34:10 -0400

    Started creating ContractRummy Cards and Card classes

---
+++ lib/ContractRummy.php
@@ -0,0 +1,57 @@
+<?php
+
+// Trying to figure out if I need a "Player" class or to contain in this class
+// Logic dictages Yes I do - though might not need it now.
+class ContractRummy extends Cards
+{
+	function ContractRummy( $players = 2)
+	{
+		
+	}
+}
+
+class Cards
+{
+	function Cards( $decks = 1, $jokers = 0 )
+	{
+	
+	}
+	
+	/**
+	 * Shuffle cards to create random order
+	 *
+	 * @return void
+	 */
+	function shuffle()
+	{
+		
+	}
+	
+	/**
+	 * Deal one card from top of the deck
+	 *
+	 * @return Card
+	 */
+	function deal()
+	{
+	
+	}
+	
+	/**
+	 * Total cards left
+	 *
+	 * @return int
+	 */
+	function total()
+	{
+		
+	}
+}
+
+class Card
+{
+	function Card()
+	{
+		
+	}
+}

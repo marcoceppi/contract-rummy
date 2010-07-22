06841fd15d5cca37b792bdb4ec81ddb57401b528
Marco Ceppi <mceppi@danya.com>
2010-07-22 11:47:35 -0400

    Began building constructor class

--- lib/ContractRummy.php
+++ lib/ContractRummy.php
@@ -12,13 +12,29 @@ class ContractRummy extends Cards
 
 class Cards
 {
-	protected $deck = array();
 	private $suites = array("clubs", "diamonds", "spades", "hearts");
 	private $faces  = array("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
 	
+	protected $playing_deck = array();
 	function Cards( $decks = 1, $jokers = 0 )
 	{
-	
+		$decks  = ( $decks < 1 ) ? 1 : $decks; // Lets not be silly now
+		$jokers = ( $jokers < 0 ) ? 0 : $jokers; // Really now - no negatives
+		
+		$faces_total = count($faces) + $jokers;
+		$deck_total  = $faces_total * count($suites);
+		
+		// Build the playing deck
+		for( $i = 0; $i < $decks; $i++ )
+		{
+			foreach( $faces as $face )
+			{
+				foreach( $suites as $suite )
+				{
+					$this->playing_deck[] = array( "suite" => $suite, "face" => $face );
+				}
+			}
+		}
 	}
 	
 	/**
